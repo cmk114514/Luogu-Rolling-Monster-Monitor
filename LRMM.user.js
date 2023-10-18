15b93a5c65e51bab0bff4403328beb916873e9a0
C(m,k) <119834826+cmk114514@users.noreply.github.com>
2023-10-18 11:39:40 +0800

    Add files via upload

--- LRMM.user.js
+++ LRMM.user.js
@@ -9,44 +9,50 @@
 // @grant        none
 // ==/UserScript==
 
-const special = {
-    127949: 'Brown',
+const LRMM_special = {
+    127949: 'brown',
+    169574: 'blackred',
 }
 
-const color = {
-    'Brown': 'color: #996600 !important',
-    'Gray': 'color: #bbb !important',
-    'Red': 'color: #e74c3c !important',
-    'Yellow': 'color: #f1c40f !important',
-    'Orange': 'color: #e67e22 !important',
-    'Purple': 'color: #8e44ad !important',
-    'Green': 'color: #5eb95e !important',
-    'Blue': 'color: #0e90d2 !important',
-}
+const LRMM_style = `
+.username {font-weight: bold; display: inline-block}
+.LRMM_brown {color: #996600 !important}
+.LRMM_gray {color: #bbb !important}
+.LRMM_red, .LRMM_blackred {color: #e74c3c !important}
+.LRMM_blackred:first-letter {color: #34495e !important}
+.LRMM_yellow {color: #f1c40f !important}
+.LRMM_orange {color: #e67e22 !important}
+.LRMM_purple {color: #8e44ad !important}
+.LRMM_green {color: #5eb95e !important}
+.LRMM_blue {color: #0e90d2 !important}
+`
 
 const LRMM_click = (h, div) => {
     const uid = 173951, cnt = 20, pid = location.href.split('/')[location.href.split('/').length - 1]
     $.get(`https://www.luogu.com.cn/user/${uid}`, e => {
         const n = JSON.parse(decodeURIComponent(/(?<=JSON\.parse\(decodeURIComponent\(").*(?=")/.exec(e)[0])).currentData.user.followingCount
         var para = document.createElement('p'), num = document.createElement('span'); num.innerText = '0'; h.appendChild(num); h.appendChild(document.createTextNode('/' + n))
-        div.insertBefore(para, h.nextSibling)
-        const A = i => {$.ajax({'url': `https://www.luogu.com.cn/api/user/followings?user=${uid}&page=${i}`, 'success': (e, _) => {
-            const B = (id, name, col) => {$.ajax({'async': false, 'url': `https://www.luogu.com.cn/record/list?user=${id}&pid=${pid}&page=1&status=12&_contentOnly=1`, 'success': (d) => {
-                if(d.currentData.records.count > 0){
-                    var span = document.createElement('span')
-                    var link = document.createElement('a')
-                    link.setAttribute('class', 'username')
-                    link.setAttribute('href', `https://www.luogu.com.cn/record/list?user=${id}&pid=${pid}&page=1&status=12`)
-                    link.innerText = e.users.result[j].name
-                    link.setAttribute('style', 'font-weight: bold;' + color[id in special? special[id]: col])
-                    span.appendChild(link)
-                    span.appendChild(document.createTextNode(', '))
-                    para.appendChild(span)
-                }
-                num.innerText++
-            }, 'error': () => {setTimeout(() => {B(id, name, col)}, 1000)}})}
-            for(var j in e.users.result) B(e.users.result[j].uid, e.users.result[j].name, e.users.result[j].color)
-        }, 'error': () => {setTimeout(() => {A(i)}, 1000)}})}
+        var style = document.createElement('style'); style.innerHTML = LRMM_style; para.appendChild(style); div.insertBefore(para, h.nextSibling)
+        const A = i => {$.ajax({
+            'url': `https://www.luogu.com.cn/api/user/followings?user=${uid}&page=${i}`,
+            'success': (e, _) => {
+                const B = (id, name, col) => {$.ajax({
+                    'async': false,
+                    'url': `https://www.luogu.com.cn/record/list?user=${id}&pid=${pid}&page=1&status=12&_contentOnly=1`,
+                    'success': (d) => {
+                        if(d.currentData.records.count > 0){
+                            var span = document.createElement('span')
+                            span.innerHTML = `<a href=https://www.luogu.com.cn/record/list?user=${id}&pid=${pid}&page=1&status=12 class="username LRMM_${id in LRMM_special? LRMM_special[id]: col}">${name}</a>, `
+                            para.appendChild(span)
+                        }
+                        num.innerText++
+                    },
+                    'error': () => {setTimeout(() => {B(id, name, col)}, 1000)}
+                })}
+                for(var j in e.users.result) B(e.users.result[j].uid, e.users.result[j].name, e.users.result[j].color.toLowerCase())
+            },
+            'error': () => {setTimeout(() => {A(i)}, 1000)}
+        })}
         for(var i = 1; i * cnt < n + cnt - 1; i++) A(i)
     })
 }
